4383bde4e15d0e54a017d5161aaa72b2e9093081
Administrator <Administrator@smart20102>
2017-08-08 15:45:39 +0900

    한글 인코딩을 인터셉터에서 해주는 코드 추가

--- pineapple/src/main/java/com/pineapple/funding/CommonInterceptor.java
+++ pineapple/src/main/java/com/pineapple/funding/CommonInterceptor.java
@@ -10,6 +10,8 @@ public class CommonInterceptor extends HandlerInterceptorAdapter {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		super.preHandle(request, response, handler);
+		// 모든 요청에 대해 한글인코딩
+		request.setCharacterEncoding("utf-8");
 		
 		// 여기서 로그인 안했을시 튕겨내는 것 추가해주고...
 		// 로그인 필요없는 페이지는 따로 분기해줌...
