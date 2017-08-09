37dde4c56858365285dbd1f70c88ebd8536b91ec
Administrator <Administrator@smart20102>
2017-08-09 09:56:18 +0900

    펀딩컨트롤러 주석처리

--- pineapple/src/main/java/com/pineapple/funding/funding/FundingController.java
+++ pineapple/src/main/java/com/pineapple/funding/funding/FundingController.java
@@ -9,26 +9,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class FundingController {
-	//
+	// pmsmain.jsp 뷰 요청
 	@RequestMapping(value = "/pmsmain.pms", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		return "./pms/pmsmain";
 	} 
-	
+	// 펀딩개설하기 페이지 요청
 	@RequestMapping(value = "/fundingopen.pms", method = RequestMethod.GET)
 	public String fundingopen(Locale locale, Model model) {
 		return "./pms/funding/fundingopen";
 	}
-	
+	// 펀딩상세정보입력 페이지 요청
 	@RequestMapping(value = "/fundingdetail.pms", method = RequestMethod.GET)
 	public String fundingdetail(Locale locale, Model model) {
 		return "./pms/funding/fundingdetail";
 	}
-	
+	// 마일스톤입력 페이지 요청
 	@RequestMapping(value = "/milestoneinsert.pms", method = RequestMethod.GET)
 	public String milestoneinsert(Locale locale, Model model) {
 		return "./pms/funding/msinsertform";
 	}
+	// wbsplan입력 페이지 요청
 	@RequestMapping(value = "/wbsplan.pms", method = RequestMethod.GET)
 	public String wbsplan(Locale locale, Model model) {
 		return "./pms/funding/wbsplan";
